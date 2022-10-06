72e1bc6379c8b8df629d61406b1e0ffb5bcd268b
misael-reyes <misaelstark11@gmail.com>
2022-10-06 18:49:29 -0500

    view binding

--- app/src/main/java/com/example/currencyexchangeapp/ui/home/HomeActivity.java
+++ app/src/main/java/com/example/currencyexchangeapp/ui/home/HomeActivity.java
@@ -3,14 +3,22 @@ package com.example.currencyexchangeapp.ui.home;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 import com.example.currencyexchangeapp.R;
+import com.example.currencyexchangeapp.databinding.ActivityHomeBinding;
 
 public class HomeActivity extends AppCompatActivity {
+
+    // esto es para el viewBinding
+    private ActivityHomeBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
+        // conectamos el view binding
+        binding = ActivityHomeBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
     }
 }
