fd1ae7eb6d9eb2889efbbbee6d732ba14092edff
misael-reyes <misaelstark11@gmail.com>
2022-10-07 19:51:54 -0500

    dark theme demo

--- app/src/main/java/com/example/currencyexchangeapp/ui/home/HomeActivity.java
+++ app/src/main/java/com/example/currencyexchangeapp/ui/home/HomeActivity.java
@@ -1,8 +1,11 @@
 package com.example.currencyexchangeapp.ui.home;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 
 import com.example.currencyexchangeapp.R;
@@ -20,5 +23,42 @@ public class HomeActivity extends AppCompatActivity {
         binding = ActivityHomeBinding.inflate(getLayoutInflater());
         View view = binding.getRoot();
         setContentView(view);
+
+//        binding.sw1.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                if (binding.sw1.isChecked())
+//                    setDayNight(0);
+//                else
+//                    setDayNight(1);
+//            }
+//        });
+    }
+
+    public void setDayNight(int theme){
+        //SharedPreferences sp = getSharedPreferences("SP", this.MODE_PRIVATE);
+        //int theme = sp.getInt("Theme", 1);
+        if(theme==0){
+            getDelegate().setLocalNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+        }
+        else{
+            getDelegate().setLocalNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+        }
+    }
+
+    // metodo para mostrar el menu
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_theme, menu);
+        return true;
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.item_theme) {
+            setDayNight(0);
+        } else {
+            setDayNight(1);
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
