80db46b68829d98073196983ad60210dce1544c7
misael-reyes <misaelstark11@gmail.com>
2022-11-09 14:28:05 -0600

    flags

--- app/src/main/java/com/example/currencyexchangeapp/ui/home/HomeActivity.java
+++ app/src/main/java/com/example/currencyexchangeapp/ui/home/HomeActivity.java
@@ -8,6 +8,7 @@ import androidx.appcompat.app.AppCompatDelegate;
 import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -20,11 +21,15 @@ import androidx.appcompat.widget.Toolbar;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.example.currencyexchangeapp.R;
+//import com.example.currencyexchangeapp.databinding.ActivityHomeBinding;
 import com.example.currencyexchangeapp.databinding.ActivityHomeBinding;
 import com.mynameismidori.currencypicker.CurrencyPicker;
 import com.mynameismidori.currencypicker.CurrencyPickerListener;
+import com.mynameismidori.currencypicker.ExtendedCurrency;
 
 public class HomeActivity extends AppCompatActivity {
+
+    private ExtendedCurrency currencyPais1, currencyPais2;
 
     // esto es para el viewBinding
     private ActivityHomeBinding binding;
@@ -32,6 +37,9 @@ public class HomeActivity extends AppCompatActivity {
     // atributos para el archivo sharedpreference
     private SharedPreferences sp;
     private SharedPreferences.Editor editor;
+
+    // picker currency
+    private CurrencyPicker picker;
 
     // viewmodel
     HomeViewModel viewModel;
@@ -42,8 +50,7 @@ public class HomeActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         // conectamos el view binding
         binding = ActivityHomeBinding.inflate(getLayoutInflater());
-        View view = binding.getRoot();
-        setContentView(view);
+        setContentView(binding.getRoot());
 
         // iniciamos las configuraciones
 
@@ -65,8 +72,20 @@ public class HomeActivity extends AppCompatActivity {
         // inicializamos el viewmodel
         viewModel = new ViewModelProvider(this).get(HomeViewModel.class);
 
+        // inicializamos el currency picker
+        picker = CurrencyPicker.newInstance("Select Currency");  // dialog title
+
         // colocamos el tema
         setDayNight();
+
+        setFlagDefault();
+
+    }
+
+    @SuppressLint("ResourceType")
+    private void setFlagDefault() {
+        binding.btnPais1.setIconResource(2131165344);
+        binding.btnPais2.setIconResource(2131165376);
     }
 
     private void initListeners() {
@@ -82,17 +101,77 @@ public class HomeActivity extends AppCompatActivity {
         binding.btnPais1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                //viewModel.agregar();
-                CurrencyPicker picker = CurrencyPicker.newInstance("Select Currency");  // dialog title
+                // evento al seleccionar una divisa del currency picker
                 picker.setListener(new CurrencyPickerListener() {
                     @Override
                     public void onSelectCurrency(String name, String code, String symbol, int flagDrawableResID) {
                         // Implement your code here
+                        currencyPais1 = new ExtendedCurrency();
+                        currencyPais1.setName(name);
+                        currencyPais1.setCode(code);
+                        currencyPais1.setSymbol(symbol);
+                        currencyPais1.setFlag(flagDrawableResID);
+
+                        configurationBtnPais1(code, flagDrawableResID);
+                        picker.dismiss();
                     }
                 });
                 picker.show(getSupportFragmentManager(), "CURRENCY_PICKER");
             }
         });
+
+        binding.btnPais2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // evento al seleccionar una divisa del currency picker
+                picker.setListener(new CurrencyPickerListener() {
+                    @Override
+                    public void onSelectCurrency(String name, String code, String symbol, int flagDrawableResID) {
+                        // Implement your code here
+                        currencyPais2 = new ExtendedCurrency();
+                        currencyPais2.setName(name);
+                        currencyPais2.setCode(code);
+                        currencyPais2.setSymbol(symbol);
+                        currencyPais2.setFlag(flagDrawableResID);
+
+                        configurationBtnPais2(code, flagDrawableResID);
+                        picker.dismiss();
+                    }
+                });
+                picker.show(getSupportFragmentManager(), "CURRENCY_PICKER");
+            }
+        });
+
+        binding.btnArrows.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                revertCountry();
+            }
+        });
+    }
+
+    private void revertCountry() {
+        if (currencyPais1 == null)
+            currencyPais1 = ExtendedCurrency.getCurrencyByName("Mexico Peso");
+        if(currencyPais2 == null)
+            currencyPais2 = ExtendedCurrency.getCurrencyByName("United States Dollar");
+
+        configurationBtnPais1(currencyPais2.getCode(), currencyPais2.getFlag());
+        configurationBtnPais2(currencyPais1.getCode(), currencyPais1.getFlag());
+
+        ExtendedCurrency aux = currencyPais2;
+        currencyPais2 = currencyPais1;
+        currencyPais1 = aux;
+    }
+
+    private void configurationBtnPais1(String code, int flag) {
+        binding.btnPais1.setText(code);
+        binding.btnPais1.setIconResource(flag);
+    }
+
+    private void configurationBtnPais2(String code, int flag) {
+        binding.btnPais2.setText(code);
+        binding.btnPais2.setIconResource(flag);
     }
 
     private void initObserver() {
